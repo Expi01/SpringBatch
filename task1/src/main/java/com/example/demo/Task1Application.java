63f652c8c862f60fa457b1df4529aa129b97da35
Expi01 <76592080+Expi01@users.noreply.github.com>
2024-07-26 01:41:01 +0530

    Update Task1Application.java

--- task1/src/main/java/com/example/demo/Task1Application.java
+++ task1/src/main/java/com/example/demo/Task1Application.java
@@ -1,13 +1,18 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Task1Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Task1Application.class, args);
-	}
+    private static final Logger logger = LoggerFactory.getLogger(Task1Application.class);
 
+    public static void main(String[] args) {
+        logger.info("Starting Task1Application...");
+        SpringApplication.run(Task1Application.class, args);
+        logger.info("Task1Application started successfully.");
+    }
 }
