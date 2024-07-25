dffb3fcd6343ef318b8ac516665c3c29f073aa3f
Expi01 <76592080+Expi01@users.noreply.github.com>
2024-07-26 01:39:05 +0530

    Update Processor.java

--- task1/src/main/java/batch/Processor.java
+++ task1/src/main/java/batch/Processor.java
@@ -1,5 +1,7 @@
 package batch;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.batch.item.adapter.ItemProcessorAdapter;
 import org.springframework.stereotype.Component;
 
@@ -10,25 +12,23 @@ import model.Employee;
 @Component
 public class Processor extends ItemProcessorAdapter<Employee, Employee> {
 
-	private static final Map<String, String> DEPT_NAMES = new HashMap<>();
-	
-	public Processor() {
-		DEPT_NAMES.put("001", "Technology");
-		DEPT_NAMES.put("002", "Operations");
-		DEPT_NAMES.put("003", "Accounts");
-	}
-	
-	@Override
-	public Employee process(Employee employee) throws Exception {
-		
-		String deptCode = employee.getDept();
-		String dept = DEPT_NAMES.get(deptCode);
-		employee.setDept(dept);
+    private static final Logger logger = LoggerFactory.getLogger(Processor.class);
 
-		System.out.println(String.format(deptCode, dept));
-//		System.out.println(employee);
-		
-		return employee;
-	}
+    private static final Map<String, String> DEPT_NAMES = new HashMap<>();
 
+    public Processor() {
+        DEPT_NAMES.put("001", "Technology");
+        DEPT_NAMES.put("002", "Operations");
+        DEPT_NAMES.put("003", "Accounts");
+    }
+
+    @Override
+    public Employee process(Employee employee) throws Exception {
+        String deptCode = employee.getDept();
+        String dept = DEPT_NAMES.get(deptCode);
+        employee.setDept(dept);
+
+        logger.info("Converted deptCode {} to dept {}", deptCode, dept);
+        return employee;
+    }
 }
