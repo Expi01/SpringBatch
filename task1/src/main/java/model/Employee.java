fe56638f8708bf4491e371433d6789893a5e4895
Expi01 <76592080+Expi01@users.noreply.github.com>
2024-07-26 01:43:08 +0530

    Update Employee.java

--- task1/src/main/java/model/Employee.java
+++ task1/src/main/java/model/Employee.java
@@ -6,63 +6,47 @@ import jakarta.persistence.Id;
 @Entity
 public class Employee {
 
-	@Id
-	private Integer id;
-	private String name;
-	private String dept;
-	private Integer salary;
-	
-	public Employee(Integer id, String name, String dept, Integer salary) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.dept = dept;
-		this.salary = salary;
-	}
+    @Id
+    private int id;
+    private String name;
+    private String dept;
+    private double salary;
 
-	public Integer getId() {
-		return id;
-	}
+    // Getters and setters
+    public int getId() {
+        return id;
+    }
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
+    public void setId(int id) {
+        this.id = id;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public String getDept() {
-		return dept;
-	}
+    public String getDept() {
+        return dept;
+    }
 
-	public void setDept(String dept) {
-		this.dept = dept;
-	}
+    public void setDept(String dept) {
+        this.dept = dept;
+    }
 
-	public Integer getSalary() {
-		return salary;
-	}
+    public double getSalary() {
+        return salary;
+    }
 
-	public void setSalary(Integer salary) {
-		this.salary = salary;
-	}
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
 
-	@Override
-	public String toString() {
-		final StringBuffer sb = new StringBuffer("Employee{");
-		sb.append("id=").append(id);
-		sb.append(", name='").append(name).append('\'');
-		sb.append(", dept='").append(dept).append('\'');
-		sb.append(", salary='").append(salary);
-		sb.append('}');
-		return sb.toString();
-	}
-
-	
-	     
+    @Override
+    public String toString() {
+        return "Employee [id=" + id + ", name=" + name + ", dept=" + dept + ", salary=" + salary + "]";
+    }
 }
