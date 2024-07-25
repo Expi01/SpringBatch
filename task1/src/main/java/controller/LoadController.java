b2fc5cfcba9ed963f1f22dd9d4f6f8e2a63424af
Expi01 <76592080+Expi01@users.noreply.github.com>
2024-07-26 01:42:31 +0530

    Update LoadController.java

--- task1/src/main/java/controller/LoadController.java
+++ task1/src/main/java/controller/LoadController.java
@@ -1,5 +1,7 @@
 package controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
@@ -15,37 +17,34 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
 import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/load")
 public class LoadController {
-	
-	@Autowired
-	JobLauncher jobLauncher;
-	
-	@Autowired
-	Job job;
-	
-	@GetMapping
-	public BatchStatus load() throws JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException, JobParametersInvalidException { 
-		
-		
-		
-		Map<String, JobParameter<?>> maps = new HashMap<>();
-		maps.put("time", new JobParameter(System.currentTimeMillis(), null));
-		JobParameters parameters=new JobParameters(maps);
-		JobExecution jobExecution = jobLauncher.run(job, parameters);
-		
-		System.out.println("JobExecution: " +jobExecution.getStatus());
-		
-		System.out.println("Batch is Running..");
-		while(jobExecution.isRunning()) {
-			System.out.println("...");
-		}
-		
-		return jobExecution.getStatus();
-	}
 
+    private static final Logger logger = LoggerFactory.getLogger(LoadController.class);
+
+    @Autowired
+    JobLauncher jobLauncher;
+
+    @Autowired
+    Job job;
+
+    @GetMapping
+    public BatchStatus load() throws JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException, JobParametersInvalidException {
+        Map<String, JobParameter<?>> maps = new HashMap<>();
+        maps.put("time", new JobParameter(System.currentTimeMillis(), null));
+        JobParameters parameters = new JobParameters(maps);
+        JobExecution jobExecution = jobLauncher.run(job, parameters);
+
+        logger.info("JobExecution: {}", jobExecution.getStatus());
+        logger.info("Batch is Running...");
+        while (jobExecution.isRunning()) {
+            logger.info("...");
+        }
+
+        return jobExecution.getStatus();
+    }
 }
