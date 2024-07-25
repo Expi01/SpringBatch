106fcc23b2aa6b617b22e76891a9a7a5d4ec00e6
Expi01 <76592080+Expi01@users.noreply.github.com>
2024-07-26 01:38:31 +0530

    Update DBWriter.java

--- task1/src/main/java/batch/DBWriter.java
+++ task1/src/main/java/batch/DBWriter.java
@@ -1,28 +1,28 @@
 package batch;
 
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.batch.item.Chunk;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
 import model.Employee;
 import repository.EmployeeRepository;
 
 @Component
 public class DBWriter implements ItemWriter<Employee> {
 
-	@Autowired
-	private EmployeeRepository employeeRepository;
-	
-	
-	@Override
-	public void write(Chunk<? extends Employee> employee) throws Exception {
-		
-		System.out.println("Data Saved for Employees: " + employee);
-		employeeRepository.saveAll(employee);
-		
-	}
+    private static final Logger logger = LoggerFactory.getLogger(DBWriter.class);
 
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Override
+    public void write(Chunk<? extends Employee> employees) throws Exception {
+        logger.info("Data Saved for Employees: {}", employees);
+        employeeRepository.saveAll(employees);
+    }
+
+	
 }
