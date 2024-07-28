f97a96d23a6893c44a79cd4ca1aa61e454751f68
Expi01 <76592080+Expi01@users.noreply.github.com>
2024-07-29 04:24:53 +0530

    Update Employee.java

--- task1/src/main/java/model/Employee.java
+++ task1/src/main/java/model/Employee.java
@@ -1,52 +1,66 @@
 package model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "employee")
 public class Employee {
 
-    @Id
-    private int id;
-    private String name;
-    private String dept;
-    private double salary;
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private String dept;
+	private double salary;
+	
+	
 
-    // Getters and setters
-    public int getId() {
-        return id;
-    }
+	public Employee(int id, String name, String dept, double salary) {
+		this.id = id;
+		this.name = name;
+		this.dept = dept;
+		this.salary = salary;
+	}
 
-    public void setId(int id) {
-        this.id = id;
-    }
+	// Getters and setters
+	public int getId() {
+		return id;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public void setId(int id) {
+		this.id = id;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public String getDept() {
-        return dept;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public void setDept(String dept) {
-        this.dept = dept;
-    }
+	public String getDept() {
+		return dept;
+	}
 
-    public double getSalary() {
-        return salary;
-    }
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
 
-    public void setSalary(double salary) {
-        this.salary = salary;
-    }
+	public double getSalary() {
+		return salary;
+	}
 
-    @Override
-    public String toString() {
-        return "Employee [id=" + id + ", name=" + name + ", dept=" + dept + ", salary=" + salary + "]";
-    }
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", dept=" + dept + ", salary=" + salary + "]";
+	}
 }
