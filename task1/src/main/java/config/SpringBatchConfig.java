6c4875f16bc40adb7bdd9df1ae96680ff1dc376e
Expi01 <76592080+Expi01@users.noreply.github.com>
2024-07-29 04:23:40 +0530

    Update SpringBatchConfig.java

--- task1/src/main/java/config/SpringBatchConfig.java
+++ task1/src/main/java/config/SpringBatchConfig.java
@@ -6,6 +6,7 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.ItemProcessor;
@@ -19,6 +20,7 @@ import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.transaction.PlatformTransactionManager;
 import model.Employee;
@@ -27,55 +29,49 @@ import model.Employee;
 @EnableBatchProcessing
 public class SpringBatchConfig {
 
-    private static final Logger logger = LoggerFactory.getLogger(SpringBatchConfig.class);
+	private static final Logger logger = LoggerFactory.getLogger(SpringBatchConfig.class);
 
-    @Bean
-    public Job job(JobRepository jobRepository, PlatformTransactionManager transactionManager,
-                   ItemReader<Employee> itemReader, ItemProcessor<Employee, Employee> itemProcessor,
-                   ItemWriter<Employee> itemWriter) {
+	@Bean
+	public Job job(JobRepository jobRepository, PlatformTransactionManager transactionManager,
+			ItemReader<Employee> itemReader, ItemProcessor<Employee, Employee> itemProcessor,
+			ItemWriter<Employee> itemWriter) {
 
-        Step step = new StepBuilder("Task-File-1", jobRepository)
-                .<Employee, Employee>chunk(100, transactionManager)
-                .reader(itemReader)
-                .processor(itemProcessor)
-                .writer(itemWriter)
-                .build();
+		Step step = new StepBuilder("Task-File-1", jobRepository).<Employee, Employee>chunk(100, transactionManager)
+				.reader(itemReader).processor(itemProcessor).writer(itemWriter).build();
 
-        logger.info("Step 'Task-File-1' configured.");
+		logger.info("Step 'Task-File-1' configured.");
 
-        return new JobBuilder("Task-1", jobRepository)
-                .start(step)
-                .build();
-    }
+		return new JobBuilder("Task-1", jobRepository).incrementer(new RunIdIncrementer()).start(step).build();
+	}
 
-    @Bean
-    public FlatFileItemReader<Employee> itemReader(@Value("${input}") Resource resource) {
-        FlatFileItemReader<Employee> flatFileItemReader = new FlatFileItemReader<>();
-        flatFileItemReader.setResource(resource);
-        flatFileItemReader.setName("CSV-Reader");
-        flatFileItemReader.setLinesToSkip(1);
-        flatFileItemReader.setLineMapper(lineMapper());
-        logger.info("FlatFileItemReader configured with resource: {}", resource.getFilename());
-        return flatFileItemReader;
-    }
+	@Bean
+	public FlatFileItemReader<Employee> itemReader(@Value("${input}") Resource resource) {
+		FlatFileItemReader<Employee> flatFileItemReader = new FlatFileItemReader<>();
+		flatFileItemReader.setResource(new ClassPathResource("employee.csv"));
+		flatFileItemReader.setName("CSV-Reader");
+		flatFileItemReader.setLinesToSkip(1);
+		flatFileItemReader.setLineMapper(lineMapper());
+		logger.info("FlatFileItemReader configured with resource: {}", resource.getFilename());
+		return flatFileItemReader;
+	}
 
-    @Bean
-    public LineMapper<Employee> lineMapper() {
-        DefaultLineMapper<Employee> defaultLineMapper = new DefaultLineMapper<>();
-        DelimitedLineTokenizer lineTokenizer = new DelimitedLineTokenizer();
+	@Bean
+	public LineMapper<Employee> lineMapper() {
+		DefaultLineMapper<Employee> defaultLineMapper = new DefaultLineMapper<>();
+		DelimitedLineTokenizer lineTokenizer = new DelimitedLineTokenizer();
 
-        lineTokenizer.setDelimiter(",");
-        lineTokenizer.setStrict(false);
-        lineTokenizer.setNames("id", "name", "dept", "salary");
+		lineTokenizer.setDelimiter(",");
+		lineTokenizer.setStrict(false);
+		lineTokenizer.setNames("id", "name", "dept", "salary");
 
-        BeanWrapperFieldSetMapper<Employee> fieldSetMapper = new BeanWrapperFieldSetMapper<>();
-        fieldSetMapper.setTargetType(Employee.class);
+		BeanWrapperFieldSetMapper<Employee> fieldSetMapper = new BeanWrapperFieldSetMapper<>();
+		fieldSetMapper.setTargetType(Employee.class);
 
-        defaultLineMapper.setLineTokenizer(lineTokenizer);
-        defaultLineMapper.setFieldSetMapper(fieldSetMapper);
+		defaultLineMapper.setLineTokenizer(lineTokenizer);
+		defaultLineMapper.setFieldSetMapper(fieldSetMapper);
 
-        logger.info("LineMapper configured.");
+		logger.info("LineMapper configured.");
 
-        return defaultLineMapper;
-    }
+		return defaultLineMapper;
+	}
 }
