3ecfe3938a11a77dc52110e47935254fb7119a8c
Expi01 <76592080+Expi01@users.noreply.github.com>
2024-07-29 04:22:23 +0530

    Update Processor.java

--- task1/src/main/java/batch/Processor.java
+++ task1/src/main/java/batch/Processor.java
@@ -12,23 +12,23 @@ import model.Employee;
 @Component
 public class Processor extends ItemProcessorAdapter<Employee, Employee> {
 
-    private static final Logger logger = LoggerFactory.getLogger(Processor.class);
+	private static final Logger logger = LoggerFactory.getLogger(Processor.class);
 
-    private static final Map<String, String> DEPT_NAMES = new HashMap<>();
+	private static final Map<String, String> DEPT_NAMES = new HashMap<>();
 
-    public Processor() {
-        DEPT_NAMES.put("001", "Technology");
-        DEPT_NAMES.put("002", "Operations");
-        DEPT_NAMES.put("003", "Accounts");
-    }
+	public Processor() {
+		DEPT_NAMES.put("001", "Technology");
+		DEPT_NAMES.put("002", "Operations");
+		DEPT_NAMES.put("003", "Accounts");
+	}
 
-    @Override
-    public Employee process(Employee employee) throws Exception {
-        String deptCode = employee.getDept();
-        String dept = DEPT_NAMES.get(deptCode);
-        employee.setDept(dept);
+	@Override
+	public Employee process(Employee employee) throws Exception {
+		String deptCode = employee.getDept();
+		String dept = DEPT_NAMES.get(deptCode);
+		employee.setDept(dept);
 
-        logger.info("Converted deptCode {} to dept {}", deptCode, dept);
-        return employee;
-    }
+		logger.info("Converted deptCode {} to dept {}", deptCode, dept);
+		return employee;
+	}
 }
