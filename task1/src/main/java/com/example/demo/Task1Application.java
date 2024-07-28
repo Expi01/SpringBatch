32753006fc08fb6bfb86d7b2ce8ee0a2d9fcedf4
Expi01 <76592080+Expi01@users.noreply.github.com>
2024-07-29 04:23:10 +0530

    Update Task1Application.java

--- task1/src/main/java/com/example/demo/Task1Application.java
+++ task1/src/main/java/com/example/demo/Task1Application.java
@@ -4,15 +4,19 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = "model")
+@EnableJpaRepositories(basePackages = "repository")
 public class Task1Application {
 
-    private static final Logger logger = LoggerFactory.getLogger(Task1Application.class);
+	private static final Logger logger = LoggerFactory.getLogger(Task1Application.class);
 
-    public static void main(String[] args) {
-        logger.info("Starting Task1Application...");
-        SpringApplication.run(Task1Application.class, args);
-        logger.info("Task1Application started successfully.");
-    }
+	public static void main(String[] args) {
+		logger.info("Starting Task1Application...");
+		SpringApplication.run(Task1Application.class, args);
+		logger.info("Task1Application started successfully.");
+	}
 }
